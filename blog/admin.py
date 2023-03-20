42f134342cc4e4500d2d418ce2d715212c920483
Rafnarok335 <mrafi181201@bscse.uiu.ac.bd>
2023-03-20 15:06:57 +0600

    Added Models For blogsite

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import User, BlogPost
+# Register your models here.
+
+admin.register(User)
+admin.register(BlogPost)
---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=15, unique=True)
+    password = models.CharField(max_length=15, null=False)
+    profile_picture = models.ImageField()
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now=True)
